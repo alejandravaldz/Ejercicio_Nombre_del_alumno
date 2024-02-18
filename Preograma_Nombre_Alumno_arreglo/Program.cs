bd8c21cbad49140c300e3d78de2bb461190744fc
Alejandra Valdez <valdezmoralesalejandramaciel@gmail.com>
2024-02-18 12:14:09 -0500

    Agregar archivos de proyecto.

---
+++ Preograma_Nombre_Alumno_arreglo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nombre de alumno y nota");
+Console.WriteLine("");
+
+
+Alumno alumno = new Alumno();
+
+alumno.nombre = "Juan perez";
+alumno.nota = 80;
+
+alumno.ImprimirDatos();
+
+class Alumno
+{
+    public string nombre;
+    public int nota;
+
+    public void ImprimirDatos()
+    {
+        Console.WriteLine($"su nombre es {nombre} y tiene {nota} puntos");
+    }
+}
+
+
+
